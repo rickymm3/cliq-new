7082eda9d881c594add450f0a87d014a2b76951b
rickymm3 <rickymm3@gmail.com>
2024-11-22 21:25:01 -0500

    heroku push2

---
+++ db/migrate/20241011185554_create_cliqs.rb
@@ -0,0 +1,15 @@
+class CreateCliqs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cliqs do |t|
+      t.string :name, null: false
+      t.text :description
+      t.bigint :parent_cliq_id
+      t.string :slug
+
+      t.timestamps
+    end
+
+    add_index :cliqs, :parent_cliq_id, name: "index_cliqs_on_parent_cliq_id"
+    add_index :cliqs, :slug, unique: true, name: "index_cliqs_on_slug"
+  end
+end
